e5ad8b5a1bedf541385afa8591b3d5a9d484f37f
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-04-22 23:43:22 +0700

    models: user schema

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.Model("User", UserSchema);
